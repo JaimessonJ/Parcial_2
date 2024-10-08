d4537c80dd27912e584239925857d89ab25f85a6
Jaimeson Alejandro Quintero Macias <aquinteromacias.rar@gmail.com>
2024-10-07 21:40:37 -0500

    cree variables, comence con un while con if e  else if

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rango;
+        int numJugadores = 0;
+        bool continuar = true;
+        Random aleatorio = new Random();
+
+        while (continuar)
+        {
+            Console.WriteLine("Bienvedido al juego de 'adivina el número' escoje el numero de jugadores (minimo 2 / maximo4): ");
+            numJugadores = Convert.ToInt32(Console.ReadLine());
+
+            while (numJugadores < 2 || numJugadores > 4) 
+            {
+                Console.WriteLine("Elige entre 2 a 4 jugadores; ");
+                numJugadores = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (numJugadores == 2)
+            {
+                rango = 50;
+            }
+            else if (numJugadores == 3)
+            {
+                rango = 100;
+            }
+            else if (numJugadores == 4)
+            {
+                rango = 200;
+            }
+        }
+    }
+}   
